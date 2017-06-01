b617422ff2f97aa3ab5eeb957536b40f557e2dfc
J Rogel <Jesus.Rogel@akqa.com>
2017-06-01 17:26:23 +0100

    Space v tab
    Space v tab issues

--- usage_example.py
+++ usage_example.py
@@ -18,15 +18,15 @@ def _parse_arguments():
     parser = argparse.ArgumentParser('Test decode_video python op.')
     parser.add_argument('--input_file', help='Path to the video file.')
     parser.add_argument('--output_file', default=None,
-    	help='(Optional) Path to the .npy file where the decoded frames will be stored.')
+        help='(Optional) Path to the .npy file where the decoded frames will be stored.')
     parser.add_argument('--play_video', default=False, action='store_true',
                         help='Play the extracted frames.')
     parser.add_argument('--num_frames', default=30, type=int,
-    	help='Number of frames per video (sequence length). Set to 0 for full video.')
+        help='Number of frames per video (sequence length). Set to 0 for full video.')
     parser.add_argument('--fps', default=-1, type=int,
-    	help='Framerate to which the input videos are converted. Use -1 for the original framerate.')
+        help='Framerate to which the input videos are converted. Use -1 for the original framerate.')
     parser.add_argument('--random_chunks', default=False, action='store_true',
-    	help='Grab video frames starting from a random position.')
+        help='Grab video frames starting from a random position.')
     return parser.parse_args()
 
 
@@ -60,9 +60,11 @@ if __name__ == '__main__':
     args = _parse_arguments()
     sess = tf.Session()
     f = tf.placeholder(tf.string)
-    video, h, w, seq_length = decode_video(f, args.num_frames, args.fps, args.random_chunks)
+    video, h, w, seq_length = decode_video(f, args.num_frames, args.fps,
+        args.random_chunks)
     start_time = time.time()
-    frames, seq_length_val = sess.run([video, seq_length], feed_dict={f: args.input_file})
+    frames, seq_length_val = sess.run([video, seq_length],
+        feed_dict={f: args.input_file})
     total_time = time.time() - start_time
     print('\nSuccessfully loaded video!\n'
           '\tDimensions: %s\n'
